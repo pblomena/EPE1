374bb60aa3338e677348408b65e230f39c2ee8d4
pblomena <pblomena@hotmail.com>
2018-05-30 01:12:39 -0400

    dd

---
+++ src/pages/cuarta/cuarta.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CuartaPage } from './cuarta';
+
+@NgModule({
+  declarations: [
+    CuartaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CuartaPage),
+  ],
+})
+export class CuartaPageModule {}
